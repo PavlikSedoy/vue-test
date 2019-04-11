a93ef987d7ccaefc64512f5fd98ac5c6c3e1bff9
Павел Седой <pavliksedoy@gmail.com>
2019-04-11 18:54:17 +0300

    2

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,38 @@
 <template>
   <div id="app">
-    <Todos />
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-
+import Header from './components/layout/Header'
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 
 export default {
   name: 'app',
   components: {
+    AddTodo,
+    Header,
     Todos
   },
   data() {
       return {
-          todos: [
-              {
-                  id: 1,
-                  title: 'Todo One',
-                  completed: false
-              },
-              {
-                  id: 2,
-                  title: 'Todo Two',
-                  completed: false
-              },
-              {
-                  id: 3,
-                  title: 'Todo three',
-                  completed: false
-              },
-          ]
+          todos: []
       }
+  },
+  methods: {
+      deleteTodo(id) {
+          this.todos = this.todos.filter(todo => todo.id !== id);
+      },
+      addTodo(newTodo) {
+          this.todos = [...this.todos, newTodo]
+      }
+  },
+  created() {
+
   }
 }
 </script>
@@ -49,5 +49,14 @@ export default {
   body {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555555;
+    color: #ffffff;
+    padding: 7px 20px;
+    cursor: pointer;
   }
 </style>
